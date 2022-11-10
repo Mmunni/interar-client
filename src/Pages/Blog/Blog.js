0cfe3ba86cc6eb19eb1748c43cc533a5e001c0d1
Mmunni <masumamunni20@gmail.com>
2022-11-10 10:05:10 +0600

    services-added

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import useTitle from '../../hooks/useTitle';
+import Banner from '../Home/Banner/Banner';
 
 const Blog = () => {
     useTitle('blog');
     return (
         <div>
-            <h1>blog</h1>
+            <Banner/>
+            <div className='max-w-screen-xl mx-auto py-32'>
+            <div className="title text-center">
+            <p className="font-bold theme-color py-5 text-xl">Our Blogs</p>
+            <h1 className="text_2xl md: text-3xl lg:text-6xl font-bold  pb-24">
+            News & <span className='theme-color'> Updates</span>  
+           
+            </h1>
+            </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import useTitle from '../../hooks/useTitle';
+import Banner from '../Home/Banner/Banner';
 
 const Services = () => {
     useTitle('services');
     return (
         <div>
-            <h1>services</h1>
+            <Banner/>
+            <div className='max-w-screen-xl mx-auto py-32'>
+            <div className="title text-center">
+            <p className="font-bold theme-color py-5 text-xl">Our Services</p>
+            <h1 className="text_2xl md: text-3xl lg:text-6xl font-bold  pb-24">
+            We Provide  <span className='theme-color'> Interior</span>  <br/>
+            Design Services
+            </h1>
+            </div>
+            </div>
         </div>
     );
 };
